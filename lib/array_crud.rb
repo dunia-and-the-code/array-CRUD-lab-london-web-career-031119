8dbbde11aa30394ea4c4106e7fbd17dd19c3237d
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-02-28 00:17:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  gelato = [coffee, fiore di late, vanilla, chocolate]
+  gelato = ["coffee", "fiore di late", "vanilla", "chocolate"]
 end
 
 def add_element_to_end_of_array(array, element)
